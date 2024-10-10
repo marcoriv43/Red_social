fc39b0ded5c250d1126fb2b9ead9f0eeda3c0e95
Marco R. <marriv4338@gmail.com>
2024-10-09 22:36:02 -0400

    Co-authored-by: Victoria Rangel <vickyniee@users.noreply.github.com>

--- app.js
+++ app.js
@@ -26,12 +26,7 @@ app.use('/users', usersRouter);
 app.use('/posts', postRoutes);
 app.use('/friendships', friendshipRoutes);
 
-
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-    console.log(`Servidor corriendo en el puerto ${port}`);
-});
-
+//MIDDLEWARE
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- controllers/friendshipController.js
+++ controllers/friendshipController.js
@@ -1,16 +1,40 @@
+const amistades=[
+    { userId: "19", friendId: "20"},
+    { userId: "20", friendId: "7"},
+]
+
 class FriendshipController {
-    // Enviar solicitud de amistad
-    sendFriendRequest(req, res) {
+  
+    sendFriendRequest (req, res) {
         const { userId, friendId } = req.body;
-        // Aquí puedes agregar la lógica para enviar una solicitud de amistad
+        // Buscar friendID de la solicitud
+        const existingFriendship = amistades.find(userObject => userObject.userId == userId)
+        //BUSCAR USUARIO EN BASE DE DATOS
+        if (existingFriendship) {
+            res.status(400).json({ message: 'La amistad ya existe' });
+            return;
+        }
+        // Si no existe, agregarlo al arreglo
+        amistades.push({ userId, friendId });
         res.status(201).json({ message: `Solicitud de amistad enviada de ${userId} a ${friendId}` });
-    }
+        console.log(amistades)
+    };
+
 
     // Eliminar amistad
     removeFriend(req, res) {
         const { userId, friendId } = req.body;
         // Aquí eliminarías la amistad entre userId y friendId
+        const usuario1 = usuarios.find(user => user.id === userId);
+        const usuario2 = usuarios.find(user => user.id === friendId);
+        
+        if (!usuario1 || !usuario2) return res.status(404).send("Usuario no encontrado");
+        
+        usuario1.amigos = usuario1.amigos.filter(amigoId => amigoId !== friendId);
+        usuario2.amigos = usuario2.amigos.filter(amigoId => amigoId !== userId);
+        console.log(amigoId)
         res.status(200).json({ message: `Amistad entre ${userId} y ${friendId} eliminada` });
+        console.log(amistades)
     }
 
     // Mostrar feed de publicaciones de amigos
