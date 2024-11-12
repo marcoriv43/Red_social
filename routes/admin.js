667be5c7f10291200200daa06155762f1126eedf
= <=>
2024-11-12 17:52:06 -0400

    Añadiendo las rutas para los administradores

---
+++ routes/admin.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../helpers/auth');
+const adminController = require('../controllers/adminController');
+
+// Rutas de usuario
+router.get('/', auth.verifyToken, auth.verifyType, (req, res, next) => { 
+  new Promise((resolve, reject) =>{        
+    adminController.getUsers(req, (error, data)=>{        
+      if (error) {
+        reject(error);
+      } else {
+        resolve(data);
+      }
+    });
+  }).then((data)=>{
+    const ActualUser  = req.session.userId;  
+    const typeUser = req.session.typeUser;
+    res.render('admin/admin', { data, ActualUser, typeUser });                    
+  }).catch((error)=>{
+    res.render('layouts/error', {error});
+  });  
+});
